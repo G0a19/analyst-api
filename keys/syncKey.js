f99d09f2a0281a845560b2ef999af501e1b8a511
G0a19 <Howtoexp30@gmail.com>
2022-06-29 21:39:46 +0300

    key fiex

--- keys/syncKey.js
+++ keys/syncKey.js
@@ -1,6 +1,8 @@
 var cron = require("node-cron");
 const createNewKey = require("./createNewKey");
 
+createNewKey("KEY");
+
 cron.schedule("*/10 * * * *", async function () {
   createNewKey("KEY");
 });

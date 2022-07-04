c01a6d518ecc09ec070735e355acb74c5a2783a9
G0a19 <Howtoexp30@gmail.com>
2022-07-04 20:55:23 +0300

    ip update

--- routes/saveReq.js
+++ routes/saveReq.js
@@ -1,29 +1,11 @@
 const appendSheets = require("./../functions/google sheets/appendSheets");
-const { networkInterfaces } = require("os");
 
 module.exports = async (req, res, next) => {
-  const nets = networkInterfaces();
-  const results = Object.create(null); // Or just '{}', an empty object
-
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
-      if (net.family === familyV4Value && !net.internal) {
-        if (!results[name]) {
-          results[name] = [];
-        }
-        results[name].push(net.address);
-      }
-    }
-  }
-
-  console.log(Object.values(results)[0][0]);
-
   try {
     appendSheets(
       "H",
       "J",
-      Object.values(results)[0][0] ?? "null",
+      req.connection.remoteAddress,
       req.method,
       new Date().toISOString()
     );
